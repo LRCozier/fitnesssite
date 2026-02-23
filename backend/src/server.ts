4f2e0aaadf2aea0382e1a634da4a066f6029dd45
LRCozier <lrcozier@gmail.com>
2026-02-23 20:50:07 +0000

    deleted redundant files and node module duplicates

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from './app';
-
-export const startServer = () => {
-  const PORT = process.env.PORT || 4000;
-
-  app.listen(PORT, () => {
-    console.log(` API running on port ${PORT}`);
-  });
-};
